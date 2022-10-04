400aa498bace4ce2b73b0f78b2512db03ea90de8
amartingir <113341165+AritzMartin@users.noreply.github.com>
2022-10-04 13:16:02 +0200

    Ejercicio2 terminado

---
+++ Ejercicio2.py
@@ -0,0 +1,4 @@
+nombre = input('Introduce tu nombre: \n')
+print('Nombre en minuscula:', nombre.lower())
+print('Nombre en minuscula:', nombre.upper())
+print('Nombre en minuscula:', nombre.title())
